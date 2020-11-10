a67766f92542706560c9fdddc20d4835171713c0
Andrii <aonufrak7@gmail.com>
2020-11-11 00:34:21 +0200

    result.txt

---
+++ Lab_2/app.py
@@ -0,0 +1,59 @@
+import requests
+import ntplib
+from datetime import datetime
+
+
+def get_time_if_url_not_work():
+    c = ntplib.NTPClient()
+    response = c.request('0.ua.pool.ntp.org', version=3)
+    t = datetime.fromtimestamp(response.tx_time).time().strftime('%H:%M:%S %p')
+    d = datetime.fromtimestamp(response.tx_time).date().strftime('%Y-%m-%d')
+    date = {"date": d, "time": t}
+    return date
+
+
+def check_time(d):
+    if "time" in d.keys():
+        print("Time is: ", d['time'])
+    try:
+        print("Date is: ", d['date'])
+        home_work(d['time'])
+    except KeyError:
+        print("No date in response!!!")
+        raise KeyError
+
+
+def main(url=''):
+    if not url:
+        print("No URL passed to function")
+        return False
+
+    try:
+        r = requests.get(url=url)
+    except requests.exceptions.RequestException as e:
+        raise ConnectionError
+
+    if r:
+        check_time(r.json())
+    else:
+        check_time(get_time_if_url_not_work())
+    return True
+
+
+def home_work(timestamp):
+    if "AM" in timestamp:
+        print("Доброї ночі")
+        return {'time': 'night'}
+    elif "PM" in timestamp:
+        print("Доброго дня")
+        return {'time': 'day'}
+    else:
+        raise ValueError("Value timestamp is undefined")
+
+
+if __name__ == "__main__":
+    a = "="*40
+    print(a + "\nРезультат без параметрів: ")
+    main()
+    print(a + "\nРезультат з правильною URL: ")
+    main('http://date.jsontest.com/')
---
+++ Lab_2/tests/tests.py
@@ -0,0 +1,28 @@
+import unittest
+from app import main, home_work
+
+
+class TestClass(unittest.TestCase):
+    def setUp(self):
+        # Дана функція налаштовує початкові агрументи визначені лише для класу
+        self.date_url = 'http://date.jsontest.com/'
+        self.ip_url = 'http://ip.jsontest.com/'
+
+    def test_date_work_successfully(self):
+        # Перевіряєм чи функція відправювала до кінця і повернулі True
+        self.assertTrue(main(self.date_url))
+
+    def test_empty_url(self):
+        # Перевіряєм чи у функцію не було передано жодної URL
+        self.assertFalse(main())
+
+    def test_no_date_in_response(self):
+        # Перевіряємо що у відповіді відсутнє поле дата (тобто передана направильна URL)
+        with self.assertRaises(Exception):
+            main(self.ip_url)
+
+    def test_home_work(self):
+        self.assertEqual(home_work("12:34:56 AM"), {'time': 'night'})
+        self.assertEqual(home_work("12:34:56 PM"), {'time': 'day'})
+        with self.assertRaises(ValueError):
+            home_work("12:34:56")
